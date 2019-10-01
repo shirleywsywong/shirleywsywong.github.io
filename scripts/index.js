7032706d67e912ccd10a24887dfa7055eab8c4cf
Shirley Wong <shirleywsywong@gmail.com>
2019-10-01 00:21:55 -0400

    updated link urls

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,24 @@
-document.getElementById("root").onscroll = function() {fix()};
-function fix() {
+app = {};
+
+document.getElementById("root").onscroll = function() {app.fix()};
+app.fix = () => {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 450) {
         // document.getElementById("cta").className = "test";
         document.getElementById("cta").classList.add("fix");
 
     } else {
-        // document.getElementById("cta").className = "";
         document.getElementById("cta").classList.remove("fix");
+        // document.getElementById("cta").className = "";
     }
+}
+app.projectFilter = function () {
+    console.log(`button click`);
+}
+
+console.log(app.projectFilter)
+//document.getElementById("project-filter").addEventListener("click", app.projectFilter);
+document.getElementById("project-filter").addEventListener("click", test);
+
+function test() {
+    console.log("asdfasdfsadf")
 }
