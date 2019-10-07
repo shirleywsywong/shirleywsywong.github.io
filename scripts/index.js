17138f853820ca20665996f3b0686c973dcf5476
Shirley Wong <shirleywsywong@gmail.com>
2019-10-07 14:57:14 -0400

    conditional rendering on project cards

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,29 @@ app = {};
 document.getElementById("root").onscroll = function() {app.fix()};
 app.fix = () => {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 450) {
-        // document.getElementById("cta").className = "test";
         document.getElementById("cta").classList.add("fix");
 
     } else {
         document.getElementById("cta").classList.remove("fix");
-        // document.getElementById("cta").className = "";
     }
 }
-app.projectFilter = function () {
-    console.log(`button click`);
+
+app.projectFilter = () => {
+
+    const selectedCategory = document.querySelector('input[name="project"]:checked')
+    app.filterCat = selectedCategory.dataset.filter;
+    console.log(app.filterCat)
+    
 }
+document.getElementById("project-filter").addEventListener("change", app.projectFilter);
 
-console.log(app.projectFilter)
-//document.getElementById("project-filter").addEventListener("click", app.projectFilter);
-document.getElementById("project-filter").addEventListener("click", test);
-
-function test() {
-    console.log("asdfasdfsadf")
+//find the corresponding category for the selected filter
+if (app.filterCat === 'react') {
+    //render react cards
+} else if (app.filterCat === 'jQuery') {
+    //render jquery cards
+} else if (app.filterCat == 'psd') {
+    //render psd cards
+} else {
+    //render all
 }
