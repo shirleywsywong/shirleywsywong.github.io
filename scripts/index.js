dcba2dd328c658824e0435e854141cbdeaf5c366
Shirley Wong <shirleywsywong@gmail.com>
2019-10-09 17:46:56 -0400

    finished conditional rendering

--- scripts/index.js
+++ scripts/index.js
@@ -16,16 +16,60 @@ app.projectFilter = () => {
     app.filterCat = selectedCategory.dataset.filter;
     console.log(app.filterCat)
     
+    app.selectProject();
 }
 document.getElementById("project-filter").addEventListener("change", app.projectFilter);
 
-//find the corresponding category for the selected filter
-if (app.filterCat === 'react') {
-    //render react cards
-} else if (app.filterCat === 'jQuery') {
-    //render jquery cards
-} else if (app.filterCat == 'psd') {
-    //render psd cards
-} else {
-    //render all
+app.selectProject = () => {
+
+
+    
+    if (app.filterCat === 'react') {
+        //render react cards
+        let unwanted = document.querySelectorAll('[data-category]:not([data-category="react"])')
+        for (let i=0; i < unwanted.length; i++) {
+            unwanted[i].style.display = 'none';
+        }
+
+        let wanted = document.querySelectorAll('[data-category="react"]')
+        for (let i = 0; i < wanted.length; i++) {
+            wanted[i].style.display = 'block';
+        }
+        
+    } else if (app.filterCat === 'jQuery') {
+        //render jquery cards
+        let unwanted = document.querySelectorAll('[data-category]:not([data-category="jquery"])')
+        for (let i = 0; i < unwanted.length; i++) {
+            unwanted[i].style.display = 'none';
+        }
+
+        let wanted = document.querySelectorAll('[data-category="jquery"]')
+        for (let i = 0; i < wanted.length; i++) {
+            wanted[i].style.display = 'block';
+        }
+
+    } else if (app.filterCat == 'psd') {
+        //render psd cards
+        let unwanted = document.querySelectorAll('[data-category]:not([data-category="psd"])')
+        for (let i = 0; i < unwanted.length; i++) {
+            unwanted[i].style.display = 'none';
+        }
+
+        let wanted = document.querySelectorAll('[data-category="psd"]')
+        for (let i = 0; i < wanted.length; i++) {
+            wanted[i].style.display = 'block';
+        }
+
+    } else {
+        //render all
+        let wanted = document.getElementsByClassName('gallery-item')
+        for (let i = 0; i < wanted.length; i++) {
+            wanted[i].style.display = 'block';
+        }
+    }
 }
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // app.selectProject();
+// });
